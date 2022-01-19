4fb8a2773361aed5d3dd3b31dc48090d946e9ad4
reiokazoeru <reiokazoeru@gmail.com>
2022-01-19 12:32:15 -0500

    made quadratic test

---
+++ quadratic.py
@@ -0,0 +1,20 @@
+from backend import Dense,Tanh,Train,mse,mse_prime,predict
+
+import inspect
+import numpy as np
+
+X = np.reshape([[0,0],[1,1],[-1,1],[0,1],[1,0],[2,4],[-3,9],[5,2]],(8,2,1))
+Y = np.reshape([[1],[1],[1],[0],[0],[1],[1],[0]],(8,1,1))
+
+network = [
+    Dense(2,3),
+    Tanh(),
+    Dense(3,1),
+    Tanh()
+]
+
+Train(network,mse,mse_prime,X,Y,epochs=1000,learning_rate=0.1)
+
+print(predict(network,[[3],[9]]))
+print(predict(network,[[1],[0]]))
+print("\n")
